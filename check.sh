a009d95597549eab0f95ac75fdd531bc6db8b5da
Kris Scott <kscott91@gmail.com>
2025-12-12 23:36:26 -0800

    Add day04a

--- check.sh
+++ check.sh
@@ -25,3 +25,4 @@ check input/day03ex.txt 3 a 357
 check input/day03.txt 3 a 17332
 check input/day03ex.txt 3 b 3121910778619
 check input/day03.txt 3 b 172516781546707
+check input/day04ex.txt 4 a 13
---
+++ src/day04.c
@@ -0,0 +1,136 @@
+#include "aoc.h"
+#include "ktl/lib/strings.h"
+
+#include <assert.h>
+#include <stdio.h>
+
+typedef struct
+{
+    strbuf buf;
+    usize width;
+} grid;
+
+static void grid_deinit(grid *const g)
+{
+    strbuf_deinit(&g->buf);
+}
+
+static usize grid_height(grid const g)
+{
+    return g.buf.len / g.width;
+}
+
+static bool
+grid_at(grid const g, i64 const row, i64 const col, char **const out)
+{
+    bool const in_bounds = 0 <= row && row <= (i64)grid_height(g) && 0 <= col &&
+                           col < (i64)g.width;
+
+    if (in_bounds)
+    {
+        i64 const idx = row * (i64)g.width + col;
+        *out = &g.buf.ptr[idx];
+    }
+
+    return in_bounds;
+}
+
+static bool
+grid_get(grid const g, i64 const row, i64 const col, char *const out)
+{
+    char *ch;
+    bool const in_bounds = grid_at(g, row, col, &ch);
+    if (in_bounds)
+    {
+        *out = *ch;
+    }
+    return in_bounds;
+}
+
+#if 0
+static bool
+grid_set(grid const g, i64 const row, i64 const col, char const value)
+{
+    char *ch;
+    bool const in_bounds = grid_at(g, row, col, &ch);
+    if (in_bounds)
+    {
+        *ch = value;
+    }
+    return in_bounds;
+}
+#endif
+
+static grid grid_from_stream(FILE *const input)
+{
+    strbuf buf = strbuf_init();
+    (void)get_line(&buf, input);
+    usize const width = buf.len;
+    while (append_stream_until(&buf, input, '\n'))
+    {
+    }
+    return (grid){
+        .buf = buf,
+        .width = width,
+    };
+}
+
+static usize grid_count_surrounding(
+    grid const g, i64 const row, i64 const col, char const match
+)
+{
+    usize count = 0;
+
+    for (isize dr = row > 0 ? -1 : 0; dr <= 1; ++dr)
+    {
+        for (isize dc = col > 0 ? -1 : 0; dc <= 1; ++dc)
+        {
+            char ch;
+            if (grid_get(g, row + dr, col + dc, &ch) && ch == match)
+            {
+                ++count;
+            }
+        }
+    }
+
+    return count;
+}
+
+i64 day04(FILE *const input, bool const b)
+{
+    (void)b;
+
+    grid g = grid_from_stream(input);
+    i64 height = (i64)grid_height(g);
+
+    i64 acc = 0;
+
+    for (i64 row = 0; row < height; ++row)
+    {
+        for (i64 col = 0; col < (i64)g.width; ++col)
+        {
+            char ch = '?';
+
+            if (grid_get(g, row, col, &ch))
+            {
+                if (ch == '@')
+                {
+                    usize count = grid_count_surrounding(g, row, col, '@');
+
+                    if (count <= 4)
+                    {
+                        ++acc;
+                        ch = 'x';
+                    }
+                }
+            }
+
+            printf("%c", ch);
+        }
+        printf("\n");
+    }
+
+    grid_deinit(&g);
+
+    return acc;
+}
